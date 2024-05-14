d755ea3a3dadfb0d698c1d675f8ab63b1cbbe0ca
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-05-14 15:13:18 +0300

    fixed square module

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
+""" Square module """
+
 
 class square():
-    
+    """ Square class """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Constructor """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation of the square """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
